9012c1e3fb2552815d9d6a8325abadcaecf65049
Stephisson Galvão <stephissongalvao@gmail.com>
2025-02-25 21:40:59 -0300

    Primeiro Commit

--- Exercicios/Program.cs
+++ Exercicios/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int notaMedia = 6;
+
+if (notaMedia >= 5)
+{
+    Console.WriteLine("Nota suficiente para aprovação");
+}
+else
+    Console.WriteLine("Reprovado");
+
+
+var linguagens = new List<string>()
+{
+    "C#",
+    "Java",
+    "JavaScript"
+};
+
+
+//  Criar uma variável chamada notaMedia e atribua um valor inteiro a ela. Caso seu valor seja maior ou igual a 5, escreva na tela "Nota suficiente para aprovação".
+//  Criar uma lista de linguagens de programação, com as linguagens C#, Java e JavaScript.
+//  Exibir o valor "C#" no console, utilizando a lista criada no exercício anterior.
+//  Criar um programa que, dado a entrada de dados de um número n inteiro, a partir do teclado, exibir a n-ésima posição de uma lista.
