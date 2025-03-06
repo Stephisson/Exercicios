6bb392fe1bb254b6ea76e534acce9c146b5cc61c
Stephisson Galvão <stephissongalvao@gmail.com>
2025-03-06 15:04:33 -0300

    Adicionado enunciado do terceiro módulo

--- Exercicios/Program.cs
+++ Exercicios/Program.cs
@@ -16,7 +16,20 @@ var linguagens = new List<string>()
 };
 
 
+//   C#: criando sua primeira aplicação C#: criando sua primeira aplicação
+
+//2 -Tipos de funções
+
 //  Criar uma variável chamada notaMedia e atribua um valor inteiro a ela. Caso seu valor seja maior ou igual a 5, escreva na tela "Nota suficiente para aprovação".
 //  Criar uma lista de linguagens de programação, com as linguagens C#, Java e JavaScript.
 //  Exibir o valor "C#" no console, utilizando a lista criada no exercício anterior.
 //  Criar um programa que, dado a entrada de dados de um número n inteiro, a partir do teclado, exibir a n-ésima posição de uma lista.
+
+
+// ***************************************************************
+//03 - Listas e Loops no C#
+
+//Escrever uma função que a partir de dois números de ponto flutuante a e b exiba no console o resultado de suas quatro operações básicas (adição, subtração, divisão e multiplicação), utilizando interpolação de strings.
+//Criar uma lista de bandas vazia e adicionar suas bandas prediletas em seguida.
+//Utilizar a estrutura 'for' para mostrar todas as suas bandas preferidas, listadas na lista do exercício anterior, no console.
+//Criar um programa que calcula a soma de todos os elementos inteiros em uma lista.
